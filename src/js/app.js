529907bd954a96eec3f49533b7f5e9f04a3419a3
Nikita-2406 <89524208686@mail.ru>
2024-12-17 21:43:00 +0300

    1

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+export function forTest() {
+  return 5;
+}
+
+const h1 = document.querySelector(".zagolovok");
+const miniText = document.querySelector(".mini_text");
+const infor = document.querySelector(".item");
+const buttonInlock = document.querySelector(".unlock");
+
+document.addEventListener("DOMContentLoaded", () => {
+  h1.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(miniText.classList.toggle("unvis"));
+    // miniText.classList.toogle("unvis")
+  });
+
+  infor.addEventListener("mouseover", () => {
+    buttonInlock.classList.remove("unvis");
+  });
+
+  infor.addEventListener("mouseout", () => {
+    buttonInlock.classList.add("unvis");
+  });
+});
